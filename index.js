904fcb5fcbdfff9adb824543f4fe28235322aafb
nathelite10 <nathlite10@gmail.com>
2023-10-05 10:18:51 +0100

    Portfolio en ligne

---
+++ index.js
@@ -0,0 +1,31 @@
+// // let nom = "ogofsqj"
+
+// // let prenom = "ihqmuigh"
+
+// // console.log("bonjour" , nom , prenom);
+
+
+// let nom_et_prenom = "Moi_même";
+
+// let age = "30"; 
+ 
+// let poste = "coach"
+
+
+// console.log("Je,,,,,,,,", nom_et_prenom , "age de" ,age , "affecte au poste de" , poste ,"atteste que");
+
+
+
+//  const monT = [1, 2, 3, 4, 5];
+
+//  for (let i = 0; i < monT; i++) {
+//     console.log(monT[i]);
+//  }
+ 
+
+function hi () {
+   console.log("Bonjour");
+}
+hi();
+
+
